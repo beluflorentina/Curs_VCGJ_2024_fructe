c572f8deccc6647d021fd8ce9089213bcb4a2cf9
Jan223 <marinica2234@gmail.com>
2024-04-24 17:20:08 +0300

    Prima versiune a aplicatiei pentru afisarea informatiilor despre Guava

--- app/443D_fructe.py
+++ app/443D_fructe.py
@@ -14,19 +14,57 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def index():
     ret = "<h1>FRUCTE</h1>"
+    ret +=f"<a href={url_for('view_guava')}>Guava - Grigoras Marin-Jan</a> <br/>" 
+
+    return ret
+
+@app.route('/guava',methods=['GET'])
+def view_guava()
+	culoare = culoare_guava()
+	descriere = descriere_guava()
+	
+	ret = "<h1>Guava</h1>"
+
+	#Links
+	ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+	ret += f"<a href={url_for('view_culoare_guava')}>[culoare]</a> | " 
+	ret += f"<a href={url_for('view_descriere_guava')}>[descriere]</a>" 
 
 
-    ret += "<pre>"
+	ret += "<h2>Descriere: </h2>"
 
-    ret += " - test1\n"
-    ret += " - test2"
+	ret += descriere
 
-    ret += "</pre>"
+	ret += "<h2>Culoare: </h2>"
 
-    ret += " - test3\n"
-    ret += " - test4"
+	ret+= culoare
 
-    return "fructe"
+	return ret
+
+@app.route('/guava/culoare',methods=['GET'])
+def view_culoare_guava()
+	culoare = culoare_guava()
+
+	ret = "<h1>Culoare Guava: </h1>"
+
+        #Links
+        ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+        ret += f"<a href={url_for('view_guava')}>[culoare]</a>" 
+	
+	return ret;
+
+@app.route('/guava/descriere',methods=['GET'])
+def view_descriere_guava()
+	descriere = descriere_guava()
+	
+	ret ="<h1>Descriere Guava:</h1>"
+
+	#Links
+	ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+        ret += f"<a href={url_for('view_guava')}>[culoare]</a>" 
+	
+	ret += descriere        
+        return ret;
 
 @app.cli.command()
 def test():
