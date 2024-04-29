ef258a25470d0213a88eeb58aacb669b1a7ce0c7
Jan223 <marinica2234@gmail.com>
2024-04-29 12:21:41 +0300

    Am dezvoltat aplicatia fructe.py

--- fructe.py
+++ fructe.py
@@ -1,43 +1,77 @@
 from flask import Flask, url_for
-
-#from app.lib import network
-#from app.lib import ubuntu
-#from app.grafice.exemplu_func_grad_2 import valori_x, valori_y, genereaza_grafice
-
-print('fructe')
-
-
+from app.lib.biblioteca_fructe import *
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     ret = "<h1>FRUCTE</h1>"
 
+    ret +=f"<a href={url_for('view_guava')}>Guava - Grigoras Marin-Jan</a> <br/>" 
 
-    ret += "<pre>"
+    return ret
 
-    ret += " - test1\n"
-    ret += " - test2"
-    
-    ret += "</pre>"
-    
-    ret += " - test3\n"
-    ret += " - test4"
-    
-    return "fructe"
-    
+@app.route('/guava',methods=['GET'])
+def view_guava():
+	culoare = culoare_guava()
+	descriere = descriere_guava()
+	
+	ret = "<h1>Guava</h1>"
+
+	#Links
+	ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+	ret += f"<a href={url_for('view_culoare_guava')}>[culoare]</a> | " 
+	ret += f"<a href={url_for('view_descriere_guava')}>[descriere]</a>" 
+
+
+	ret += "<h2>Descriere: </h2>"
+
+	ret += descriere
+
+	ret += "<h2>Culoare: </h2>"
+
+	ret+= culoare
+	
+	
+	return ret
+
+@app.route('/guava/culoare',methods=['GET'])
+def view_culoare_guava():
+	culoare = culoare_guava()
+
+	ret = "<h1>Culoare Guava: </h1>"
+	ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+	ret += f"<a href={url_for('view_descriere_guava')}>[descriere]</a>" 
+	
+	ret+= "<h2> </h2>"
+	ret+= culoare		
+	return ret;
+
+@app.route('/guava/descriere',methods=['GET'])
+def view_descriere_guava():
+	descriere = descriere_guava()
+	
+	ret ="<h1>Descriere Guava:</h1>"
+
+	#Links
+	ret += f"<a href={url_for('index')}>[fructe]</a> | " 
+	ret += f"<a href={url_for('view_culoare_guava')}>[culoare]</a>" 
+	
+	ret+= "<h2> </h2>"
+	ret += descriere        
+	return ret;
+
 @app.cli.command()
 def test():
     """
     Rulare 'unit tests'
-    
+
     Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
-    
-    """
+
+     """
     import pytest
     sys.exit(pytest.main(["."]))
     
-
-    
+if __name__ == '__main__':
+    app.run(host'127.0.0.1')
    
