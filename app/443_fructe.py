1ad0f0fc29005a61d9257c2ef58ff1a205ea515e
enakitohakamui <boggy@enakitohakamui.com>
2024-05-14 16:06:57 +0300

    feature_Bogdan_Crisan/add_ananas

--- app/443_fructe.py
+++ app/443_fructe.py
@@ -4,73 +4,73 @@ from lib.biblioteca_fructe import *
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     ret = "<h1>Fructe</h1>"
-    
-    ret += f"<a href={url_for('view_acai')}>Acai - Belu FLorentina-Alexandra</a> <br/>"
+
+    ret += f"<a href={url_for('view_ananas')}>Ananas - Crisan Ioan-Bogdan</a> <br/>"
 
     return ret
 
 
-    
-@app.route('/acai', methods=['GET'])
-def view_acai():
-	culoare = culoare_acai()
-	descriere = descriere_acai() 
-	
-	ret = "<h1>Acai</h1>"
-	
-	#Linkuri
-	ret += f"<a href={url_for('index')}>[fructe]</a> | "
-	ret += f"<a href={url_for('view_culoare_acai')}>[culoare]</a> | "
-	ret += f"<a href={url_for('view_descriere_acai')}>[descriere]</a>"
-	
-	ret += "<h2>Descriere: </h2>"
-	
-	ret += descriere 
-	
-	ret += "<h2>Culoare: </h2>"
-	ret += culoare
-	
-	
-	
-	return ret
+@app.route("/ananas", methods=["GET"])
+def view_ananas():
+    culoare = culoare_ananas()
+    descriere = descriere_ananas()
 
+    ret = "<h1>Ananas</h1>"
 
-@app.route('/acai/culoare', methods=['GET'])
-def view_culoare_acai():
-    culoare = culoare_acai()  
-    
-    ret = "<h1>Culoarea acai:</h1>"
+    # Linkuri
     ret += f"<a href={url_for('index')}>[fructe]</a> | "
-    ret += f"<a href={url_for('view_acai')}>[acai]</a> <br/> <br/>"
-    ret += culoare
-    
-    return ret
+    ret += f"<a href={url_for('view_culoare_ananas')}>[culoare]</a> | "
+    ret += f"<a href={url_for('view_descriere_ananas')}>[descriere]</a>"
 
-@app.route('/acai/descriere', methods=['GET'])
-def view_descriere_acai():
-    descriere = descriere_acai()  
-    
-    ret = "<h1>Descriere acai:</h1>"
-    ret += f"<a href={url_for('index')}>[fructe]</a> | "
-    ret += f"<a href={url_for('view_acai')}>[acai]</a> <br/> <br/>"
+    ret += "<h2>Descriere: </h2>"
+
     ret += descriere
-    
+
+    ret += "<h2>Culoare: </h2>"
+    ret += culoare
+
     return ret
-    
-    
+
+
+@app.route("/ananas/culoare", methods=["GET"])
+def view_culoare_ananas():
+    culoare = culoare_ananas()
+
+    ret = "<h1>Culoarea ananas:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_ananas')}>[ananas]</a> <br/> <br/>"
+    ret += culoare
+
+    return ret
+
+
+@app.route("/ananas/descriere", methods=["GET"])
+def view_descriere_ananas():
+    descriere = descriere_ananas()
+
+    ret = "<h1>Descriere ananas:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_ananas')}>[ananas]</a> <br/> <br/>"
+    ret += descriere
+
+    return ret
+
+
 @app.cli.command()
 def test():
     """
     Rulare 'unit tests'
-    
+
     Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
-    
+
     """
     import pytest
+
     sys.exit(pytest.main(["."]))
-    
-if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1")
--- app/test/test_lib.py
+++ app/test/test_lib.py
@@ -1,30 +1,32 @@
 import logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 import sys
+
 sys.path.append("../")
 
 import lib.biblioteca_fructe as fructe
 
 
-
-def test_culoare_acai():
-    culoare = fructe.culoare_acai()
+def test_culoare_ananas():
+    culoare = fructe.culoare_ananas()
 
     if "rich purple color" in culoare:
-        logger.info(f"Functia culoare_acai functioneaza corect: {culoare}")
+        logger.info(f"Functia culoare_ananas functioneaza corect: {culoare}")
         assert True
     else:
-        logger.error(f"Functia culoare_acai NU functioneaza corect: {culoare}")
+        logger.error(f"Functia culoare_ananas NU functioneaza corect: {culoare}")
         assert False
 
-def test_descriere_acai():
-    descriere = fructe.descriere_acai()
+
+def test_descriere_ananas():
+    descriere = fructe.descriere_ananas()
 
     if "scientifically known as Euterpe oleracea" in descriere:
-        logger.info(f"Functia descriere_acai functioneaza corect:\n{descriere}")
+        logger.info(f"Functia descriere_ananas functioneaza corect:\n{descriere}")
         assert True
     else:
-        logger.error(f"Functia descriere_acai NU functioneaza corect:\n{descriere}")
-        assert False 
+        logger.error(f"Functia descriere_ananas NU functioneaza corect:\n{descriere}")
+        assert False
