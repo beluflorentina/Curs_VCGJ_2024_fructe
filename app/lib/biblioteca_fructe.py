514526da1d5aa2bf6861f878884cd78a7800fdb4
enakitohakamui <boggy@enakitohakamui.com>
2024-05-16 00:47:17 +0300

    feature_v2_Bogdan_Crisan/add_ananas

--- app/lib/biblioteca_fructe.py
+++ app/lib/biblioteca_fructe.py
@@ -1,5 +1,6 @@
 def culoare_ananas():
     return "When you think of pineapples, you probably think about a greenish-yellow fruit. But the exterior of a pineapple changes from a green-gray to yellow as it ripens, so as a general rule, the more yellow a pineapple's exterior is, the riper the fruit will be."
+    return "/yellow as a submarine"
 
 
 def descriere_ananas():
