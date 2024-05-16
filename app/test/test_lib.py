e6b802a6e5554c17eca1defcd579a3fbf07282c5
Florin Rinciog <127497874+buzzer0996@users.noreply.github.com>
2024-05-16 16:46:48 +0300

    Update test_lib.py

--- app/test/test_lib.py
+++ app/test/test_lib.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 import sys
-sys.path.append("../")
+sys.path.append("../app/")
 
 import lib.biblioteca_fructe as fructe
 
