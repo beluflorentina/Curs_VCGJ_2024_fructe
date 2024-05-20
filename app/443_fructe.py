25e54cd0922508741d200e34b0c60e6f4b1e8303
ioana-valeria-nechita <ioana-valeria.nechita@hpe.com>
2024-05-20 17:52:30 +0300

    added ananas/Ioana-Valeria Nechita

--- app/443_fructe.py
+++ app/443_fructe.py
@@ -9,9 +9,55 @@ def index():
     ret = "<h1>Fructe</h1>"
     
     ret += f"<a href={url_for('view_acai')}>Acai - Belu FLorentina-Alexandra</a> <br/>"
+    ret += f"<a href={url_for('view_ananas')}>Ananas - Nechita Ioana-Valeria</a> <br/>"
 
     return ret
 
+@app.route('/ananas', methods=['GET'])
+def view_ananas():
+	culoare = culoare_ananas()
+	descriere = descriere_ananas()
+
+	ret = "<h1>Ananas</h1>"
+
+	#Linkuri
+	ret += f"<a href={url_for('index')}>[fructe]</a> | "
+	ret += f"<a href={url_for('view_culoare_ananas')}>[culoare]</a> | "
+	ret += f"<a href={url_for('view_descriere_ananas')}>[descriere]</a>"
+
+	ret += "<h2>Descriere: </h2>"
+
+	ret += descriere
+
+	ret += "<h2>Culoare: </h2>"
+	ret += culoare
+
+
+
+	return ret
+
+
+@app.route('/ananas/culoare', methods=['GET'])
+def view_culoare_ananas():
+    culoare = culoare_ananas()
+
+    ret = "<h1>Culoarea ananas:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_ananas')}>[ananas]</a> <br/> <br/>"
+    ret += culoare
+
+    return ret
+
+@app.route('/ananas/descriere', methods=['GET'])
+def view_descriere_ananas():
+    descriere = descriere_ananas()
+
+    ret = "<h1>Descriere ananas:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_ananas')}>[ananas]</a> <br/> <br/>"
+    ret += descriere
+
+    return ret
 
     
 @app.route('/acai', methods=['GET'])
