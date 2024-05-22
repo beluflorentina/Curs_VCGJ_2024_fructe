a5541ba49f7ec72dedc0a5c1833a866dead0c607
SparkPlug32 <mihaibusicescu@gmail.com>
2024-05-22 14:59:52 +0300

    mod 5

---
+++ app/lib/biblioteca_fructe.py
@@ -0,0 +1,5 @@
+def culoare_pruna():
+    return "Trunchiul său are un duramen (miez) foarte dur, rezistent la apă, de culoare roșu-maroniu, învelit de un alburn de culoare deschisă prin care circulă seva. Arderea sa degajă o putere calorică superioară celei a fagului, cărbunele său fiind folosit în fierării."
+
+def descriere_pruna():
+    return "Prunul (denumire științifică: Prunus domestica) este un arbore fructifer din familia rozaceelor. Fructele sale cărnoase, de culoare albastru închis sau gălbui, își găsesc multiple întrebuințări în industria alimentară, pentru conserve sau gemuri, fiind consumate și în stare proaspătă. Dar cea mai cunoscută utilizare este pentru fabricarea rachiului, uneori fiind și depozitat în butoaie fabricate din lemnul acestuia. Fructele sale imature sunt uneori întrebuințate în combinație cu murături. Crește în zone de înaltă fertilitate, preferabil deluroase. Soiurile sale cele mai rezistente sunt: ciorești, corcodane, vinete și grase (denumiri regionale). "
