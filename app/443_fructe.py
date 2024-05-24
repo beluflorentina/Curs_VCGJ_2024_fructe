a9fbdea1d8e1fc7c068f5b6af471d14bb6452c2c
MaraConstantin <cmarateodora@gmail.com>
2024-05-24 15:22:12 +0300

    Am creat rutele pentru cirese in aplicatia principala

--- app/443_fructe.py
+++ app/443_fructe.py
@@ -11,6 +11,59 @@ def index():
     ret += f"<a href={url_for('view_acai')}>Acai - Belu FLorentina-Alexandra</a> <br/>"
 
     return ret
+    
+    
+@app.route('/cirese', methods=['GET'])
+def view_cirese():
+	culoare = culoare_cirese()
+	descriere = descriere_cirese() 
+	
+	ret = "<h1>Cirese</h1>"
+	
+	#Linkuri
+	ret += f"<a href={url_for('index')}>[fructe]</a> | "
+	ret += f"<a href={url_for('view_culoare_cirese')}>[culoare]</a> | "
+	ret += f"<a href={url_for('view_descriere_cirese')}>[descriere]</a>"
+	
+	ret += "<h2>Descriere: </h2>"
+	
+	ret += descriere 
+	
+	ret += "<h2>Culoare: </h2>"
+	ret += culoare
+	
+	
+	
+	return ret
+
+
+@app.route('/cirese/culoare', methods=['GET'])
+def view_culoare_cirese():
+    culoare = culoare_cirese()  
+    
+    ret = "<h1>Culoarea cireselor:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_cirese')}>[cirese]</a> <br/> <br/>"
+    ret += culoare
+    
+    return ret
+
+@app.route('/cirese/descriere', methods=['GET'])
+def view_descriere_cirese():
+    descriere = descriere_cirese()  
+    
+    ret = "<h1>Descriere cirese:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_cirese')}>[cirese]</a> <br/> <br/>"
+    ret += descriere
+    
+    return ret
+    
+    
+    
+    
+    
+    
 
 
     
