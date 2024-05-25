6556d591ddd7de919fb7b8e84c6edd65ff11d23a
hriso <andreihrisotimos.10@gmail.com>
2024-05-25 21:03:18 +0300

    modificare2 test_lib.py

--- app/test/test_lib.py
+++ app/test/test_lib.py
@@ -12,7 +12,7 @@ import lib.biblioteca_fructe as fructe
 def test_culoare_durian():
     culoare = fructe.culoare_durian()
 
-    if "also be red or green" in culoare:
+    if "yellow or white" in culoare:
         logger.info(f"Functia culoare_durian functioneaza corect: {culoare}")
         assert True
     else:
