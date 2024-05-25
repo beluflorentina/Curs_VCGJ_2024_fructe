8706efabae03973944fe3b531ffe9696abab8aab
maaian09 <maaian.iancu@gmail.com>
2024-05-25 03:19:12 +0300

    Rutele din aplicatie pentru papaya

--- app/443_fructe.py
+++ app/443_fructe.py
@@ -9,10 +9,60 @@ def index():
     ret = "<h1>Fructe</h1>"
     
     ret += f"<a href={url_for('view_acai')}>Acai - Belu FLorentina-Alexandra</a> <br/>"
+    
+    ret += f"<a href={url_for('view_papaya')}>Papaya - Iancu Maaian</a> <br/>"
 
     return ret
+    
+    
+
+@app.route('/papaya', methods=['GET'])
+def view_papaya():
+	culoare = culoare_papaya()
+	descriere = descriere_papaya() 
+	
+	ret = "<h1>Papaya</h1>"
+	
+	#Linkuri
+	ret += f"<a href={url_for('index')}>[fructe]</a> | "
+	ret += f"<a href={url_for('view_culoare_papaya')}>[culoare]</a> | "
+	ret += f"<a href={url_for('view_descriere_papaya')}>[descriere]</a>"
+	
+	ret += "<h2>Descriere: </h2>"
+	
+	ret += descriere 
+	
+	ret += "<h2>Culoare: </h2>"
+	ret += culoare
+	
+	
+	
+	return ret
 
 
+@app.route('/papaya/culoare', methods=['GET'])
+def view_culoare_papaya():
+    culoare = culoare_papaya()  
+    
+    ret = "<h1>Culoarea papaya:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_papaya')}>[papaya]</a> <br/> <br/>"
+    ret += culoare
+    
+    return ret
+
+@app.route('/papaya/descriere', methods=['GET'])
+def view_descriere_papaya():
+    descriere = descriere_papaya()  
+    
+    ret = "<h1>Descriere papaya:</h1>"
+    ret += f"<a href={url_for('index')}>[fructe]</a> | "
+    ret += f"<a href={url_for('view_papaya')}>[papaya]</a> <br/> <br/>"
+    ret += descriere
+    
+    return ret
+    
+    
     
 @app.route('/acai', methods=['GET'])
 def view_acai():
